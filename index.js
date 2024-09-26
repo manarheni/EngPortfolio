16b98a36882d36d54c9d7c09e6368a36c7819558
unknown <manarheni9@gmail.com>
2024-09-26 21:00:03 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_hdweplj";
+    const templateID = "template_rkcs2nu";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("subject").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+  
